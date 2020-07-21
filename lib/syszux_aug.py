d4f3f687a85cb43d04abfe733f1a99726b35dce8
lihang <lihang@deepait.com>
2020-07-21 15:45:52 +0000

    update aug

--- lib/syszux_aug.py
+++ lib/syszux_aug.py
@@ -59,22 +59,110 @@ class PerspectAug(AugBase):
 
     def auditConfig(self):
         self.borderValue = (255,255,255)
+        self.sh_lower = 6
+        self.sh_upper = 11
+        self.sw_lower = 20
+        self.sw_upper = 31
 
     def __call__(self, img):
         h,w=img.shape[:2]
-        scale_h = np.random.randint(6,11)
-        scale_w = np.random.randint(20,31)
-        dirc = np.random.randint(0,4)
+        scale_h = np.random.randint(self.sh_lower ,self.sh_upper)
+        scale_w = np.random.randint(self.sw_lower ,self.sw_upper)
         point1 = np.array([[0,0],[w,0],[0,h],[w,h]],dtype = "float32")
+        point2_list = [
+            np.array([[w/scale_w,0],[(scale_w-1)*w/scale_w,0],[0,h],[w,h]],dtype = "float32"),
+            np.array([[0,h/scale_h],[w,0],[0,(scale_h-1)*h/scale_h],[w,h]],dtype = "float32"),
+            np.array([[0,0],[w,0],[w/scale_w,h],[(scale_w-1)*w/scale_w,h]],dtype = "float32"),
+            np.array([[0,0],[w,h/scale_h],[0,h],[w,(scale_h-1)*h/scale_h]],dtype = "float32")]
         
-        if dirc == 0:
-            point2 = np.array([[w/scale_w,0],[(scale_w-1)*w/scale_w,0],[0,h],[w,h]],dtype = "float32")
-        elif dirc == 1:
-            point2 = np.array([[0,h/scale_h],[w,0],[0,(scale_h-1)*h/scale_h],[w,h]],dtype = "float32")
-        elif dirc == 2:
-            point2 = np.array([[0,0],[w,0],[w/scale_w,h],[(scale_w-1)*w/scale_w,h]],dtype = "float32")
-        else:
-            point2 = np.array([[0,0],[w,h/scale_h],[0,h],[w,(scale_h-1)*h/scale_h]],dtype = "float32")
-        M = cv2.getPerspectiveTransform(point1,point2)
+        pt_idx = np.random.randint(0,4)
+        M = cv2.getPerspectiveTransform(point1,point2_list[pt_idx])
         img_perspect = cv2.warpPerspective(img,M,(w,h),borderValue=self.borderValue)
         return img_perspect
+
+class GaussianAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(GaussianAug,self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.ks = 5
+
+    def __call__(self, img):
+        img_gaussian = cv2.GaussianBlur(img,(self.ks, self.ks), 0)
+        return img_gaussian
+
+class HorlineAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(HorlineAug,self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.space = 4
+        self.color = 0
+        self.thickness = 1
+
+    def __call__(self, img):
+        img_horline = img.copy()
+        img_h, img_w = img.shape[:2]
+        for i in range(0,img_h,self.space):
+            cv2.line(img_horline, (0, i), (img_w, i), self.color, self.thickness)
+        return img_horline
+
+class VerlineAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(VerlineAug,self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.space = 4
+        self.color = 0
+        self.thickness = 1
+
+    def __call__(self, img):
+        img_verline = img.copy()
+        img_h, img_w = img.shape[:2]
+        for i in range(0,img_w,self.space):
+            cv2.line(img_verline, (i, 0), (i, img_h), self.color, self.thickness)
+        return img_verline
+
+class LRmotionAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(LRmotionAug,self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.ks = 5
+
+    def __call__(self,img):
+        kernel_motion_blur = np.zeros((self.ks, self.ks))
+        kernel_motion_blur[int((self.ks - 1) / 2), :] = np.ones(self.ks)
+        kernel_motion_blur = kernel_motion_blur / self.ks
+        img_lrmotion = cv2.filter2D(img, -1, kernel_motion_blur)
+        return img_lrmotion
+
+class UDmotionAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(UDmotionAug,self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.ks = 9
+
+    def __call__(self, img):
+        kernel_motion_blur = np.zeros((self.ks, self.ks))
+        kernel_motion_blur[:, int((self.ks - 1) / 2)] = np.ones(self.ks)
+        kernel_motion_blur = kernel_motion_blur / self.ks
+        img_udmotion = cv2.filter2D(img, -1, kernel_motion_blur)
+        return img_udmotion
+
+class NoisyAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(NoisyAug,self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.mean = 0
+        self.sigma = 1
+    
+    def __call__(self, img):
+        row, col = img.shape[:2]
+        gauss = np.random.normal(self.mean, self.sigma, (row, col,3))
+        gauss = gauss.reshape(row, col,3)
+        noisy = img + gauss
+        img_noisy= noisy.astype(np.uint8)
+        return img_noisy
