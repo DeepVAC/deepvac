34fd2ee00a704025342df4c2a2fee8da8322b6c0
lihang <lihang@deepait.com>
2020-07-26 16:26:17 +0000

    update config

--- examples/aug/config.py
+++ examples/aug/config.py
@@ -1,5 +1,5 @@
 import os
 from syszux_config import *
 ##########text synthesis########
-config.text.input_dir = '/gemfield/hostpv/gemfield/tmp_input/imgs'
-config.text.output_dir = '/gemfield/hostpv/gemfield/tmp/'
+config.text.input_dir = '/gemfield/hostpv/gemfield/input/'
+config.text.output_dir = '/gemfield/hostpv/gemfield/output/'
