9314b9ce287de5f1b405a243a210faaabac9071d
lihang <lihang@deepait.com>
2020-07-28 03:51:35 +0000

    update params

--- examples/synthesis/config.py
+++ examples/synthesis/config.py
@@ -2,8 +2,10 @@ import os
 from syszux_config import *
 ##########text synthesis########
 config.text.total_num = 100
-config.text.output_path = '/gemfield/hostpv/dataset/deepvac-ocr/pure/'
+config.text.output_dir = '/gemfield/hostpv/dataset/deepvac-ocr/output_dir/'
 config.text.txt_file = '/gemfield/hostpv/dataset/deepvac-ocr/lex.txt'
 config.text.video_file = '/gemfield/hostpv/dataset/deepvac-ocr/PARASITE.mp4'
 config.text.sample_rate = 10
+config.text.fonts_dir = '/home/lihang/dataset/Ocr1/video/font/'
+#config.text.font_dir = '/gemfield/hostpv/dataset/deepvac-ocr/font/'
 config.text.is_border = True
