b6d5f7aae41d25c4149d060a93b914649c53b161
Gemfield <gemfield@civilnet.cn>
2020-08-10 07:45:10 +0000

    Add face aug

--- lib/syszux_executor.py
+++ lib/syszux_executor.py
@@ -7,20 +7,23 @@ from syszux_aug_factory import AugFactory
 from syszux_loader_factory import LoaderFactory,DatasetFactory
 from syszux_synthesis_factory import SynthesisFactory
 import cv2
+import random
 
 class Chain(object):
     def __init__(self, flow_list):
         flow = flow_list.split("=>")
         self.op_sym_list = [x.strip() for x in flow]
-        self.op_sym_list = [x.strip() for x in self.op_sym_list if x]
+        self.p_list = [ float(x.split('@')[1].strip()) if '@' in x else 1 for x in self.op_sym_list  ]
+        self.op_sym_list = [x.split('@')[0].strip() for x in self.op_sym_list if x]
         self.op_list = []
 
     def __call__(self):
         for t in self.op_list:
             t()
     
-    def addOp(self, op):
+    def addOp(self, op, p=1):
         self.op_list.append(op)
+        self.p_list.append(p)
 
 class ProductChain(Chain):
     def __init__(self, flow_list):
@@ -30,9 +33,6 @@ class ProductChain(Chain):
         for t in self.op_list:
             img = t(img)
         return img
-    
-    def addOp(self, op):
-        self.op_list.append(op)
 
 class DeepvacChain(ProductChain):
     def __init__(self, flow_list, deepvac_config):
@@ -44,22 +44,35 @@ class AugChain(ProductChain):
         super(AugChain, self).__init__(flow_list)
         self.factory = AugFactory()
         self.op_list = [self.factory.get(x)(deepvac_config) for x in self.op_sym_list]
+
+class RandomAugChain(AugChain):
+    def __call__(self, img):
+        for t,p in zip(self.op_list, self.p_list):
+            if random.random() < p:
+                img = t(img)
+        return img
+
+class DiceAugChain(AugChain):
+    def __call__(self, img):
+        i = random.randrange(len(self.op_list))
+        return self.op_list[i](img)
         
 class Executor(object):
     def __init__(self,deepvac_config):
         self._graph = OrderedDict()
-        self.loader = None
+        self._graph_p = OrderedDict()
         self.conf = deepvac_config
         self.auditConfig()
 
-    def addAugChain(self, name, chain):
+    def addAugChain(self, name, chain, p=1.0):
         self._graph[name] = chain
+        self._graph_p[name] = p
 
     def auditConfig(self):
         pass
 
-    def addOp(self, name, op):
-        self._graph[name].addOp(op)
+    def addOp(self, name, op, p=1.0):
+        self._graph[name].addOp(op,p)
 
     def remove(self, name):
         try:
@@ -67,12 +80,22 @@ class Executor(object):
         except KeyError:
             pass
 
-    def __call__(self):
-        for f in self.loader:
-            img = cv2.imread(f)
-            for k in self._graph:
-                x = self._graph[k](img)  
+    def __call__(self, img):
+        for k in self._graph:
+            if random.random() < self._graph_p[k]:
+                img = self._graph[k](img)  
+        return img   
+              
+class FaceAugExecutor(Executor):
+    def __init__(self, deepvac_config):
+        super(FaceAugExecutor, self).__init__(deepvac_config)
 
+        ac1 = RandomAugChain('RandomColorJitterAug@0.5 => MosaicAug@0.5',deepvac_config)
+        ac2 = DiceAugChain('IPCFaceAug => MotionAug',deepvac_config)
+
+        self.addAugChain('ac1', ac1, 1)
+        self.addAugChain('ac2', ac2, 0.5)
+        
 class AugExecutor(Executor):
     def __init__(self, deepvac_config):
         super(AugExecutor, self).__init__(deepvac_config)
@@ -99,7 +122,6 @@ class AugExecutor(Executor):
         output_file_name = os.path.join(output_dir, fname)
         cv2.imwrite(output_file_name, img)
 
-
     def __call__(self):
         for f in self.loader():
             img = cv2.imread(f)
@@ -109,7 +131,6 @@ class AugExecutor(Executor):
                 self.dumpImgToPath(k, f, out)
 
 if __name__ == "__main__":
-    from config import config as deepvac_config
-    executor = AugExecutor(deepvac_config)
-    executor()
-    
+    x = Chain("RandomColorJitterAug@0.3 => MosaicAug@0.8 => MotionAug ")
+    print(x.op_sym_list)
+    print(x.p_list)
