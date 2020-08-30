52953f59321bc0a5938fa47184db5712a44affec
liubaojia <bjliu@deepait.com>
2020-08-30 02:20:00 +0000

    add getDatasetRepeat

--- examples/getDatasetReapt/config.py
+++ examples/getDatasetReapt/config.py
@@ -2,5 +2,5 @@ import os
 from deepvac.syszux_config import *
 ##########classifier report########
 config.repeat = AttrDict()
-config.repeat.input_path = '/gemfield/hostpv/wangyuhang/report_split/emor_emor_all.txt'
-config.repeat.output_path = '/gemfield/hostpv/wangyuhang/report_split/emor_repeat_new2.txt'
+config.repeat.input_path = '/ your input file path /'
+config.repeat.output_path = '/ your output file path /'
