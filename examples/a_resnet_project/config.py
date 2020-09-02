5fb27e7abe08990b8502e76e1414f54f37e92350
Gemfield <gemfield@civilnet.cn>
2020-09-02 11:53:54 +0000

    add test emb

--- examples/a_resnet_project/config.py
+++ examples/a_resnet_project/config.py
@@ -3,6 +3,7 @@ from deepvac.syszux_config import *
 from torchvision import transforms
 
 # global
+config.disable_git = True
 config.lr = 1e-3
 config.cls_num = 3
 config.epoch_num = 50
@@ -13,7 +14,7 @@ config.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # train
 config.train.shuffle = True
 config.train.batch_size = 96
-config.train.img_folder = "/gemfield/hostpv/nsfw/porn_cls3/train/"
+config.train.img_folder = "/gemfield/hostpv/nsfw/train/"
 config.train.transform_op = transforms.Compose([transforms.Resize(config.input_size),
                                                   transforms.ToTensor(),
                                                   transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
@@ -22,14 +23,17 @@ config.train.transform_op = transforms.Compose([transforms.Resize(config.input_s
 # val
 config.val.shuffle = True
 config.val.batch_size = 100
-config.val.img_folder = "/gemfield/hostpv/nsfw/porn_cls3/val/"
+config.val.img_folder = "/gemfield/hostpv/nsfw/val/"
 config.val.transform_op = transforms.Compose([transforms.Resize(config.input_size),
                                                 transforms.ToTensor(),
                                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                                                 ])
 
 # test
+config.model_path = '/root/.cache/torch/hub/checkpoints/resnet50-19c8e357.pth'
 config.test.ds_name = "gemfield"
-config.test.cls_to_idx = ['neutral', 'porn', 'sexy']
-config.test.input_dir = "/gemfield/hostpv/nsfw/porn_cls3/test/"
-config.test.model_path = "output/LTS_NSFW_train_standard/model:2020-08-17-10-05_acc:0.877159855021952_epoch:0_step:332_lr:0.001.pth"
+config.test.input_dir = "/gemfield/hostpv/nsfw/test/"
+config.test.transform_op = transforms.Compose([transforms.Resize(config.input_size),
+                                                transforms.ToTensor(),
+                                                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                                ])
