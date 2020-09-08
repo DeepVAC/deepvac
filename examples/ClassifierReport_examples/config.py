2dde02d689d6046e4ef0960e796764216328b98a
wyh163 <317488556@qq.com>
2020-09-08 10:15:56 +0800

    add getDatasetRepeat (#28)
    * add_classifier_examples
    
    * add_classifier_examples
    
    * classifier fix
    
    * classifier fix
    
    * classifier fix
    
    * classifier fix
    
    * classifier fix
    
    * classifier fix
    
    * conflict fix
    
    * add getDatasetRepeat
    
    * add getDatasetRepeat
    
    * add getDatasetRepeat
    
    * add faiss report
    
    * add get_repeat
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add feature_vector
    
    Co-authored-by: liubaojia <bjliu@deepait.com>

--- examples/ClassifierReport_examples/config.py
+++ examples/ClassifierReport_examples/config.py
@@ -2,9 +2,24 @@ import os
 from deepvac.syszux_config import *
 ##########classifier report########
 config.cls = AttrDict()
-config.cls.file_path = './test_report.txt'
-config.cls.cls_num = 90219
-config.cls.log_every = 10000
-config.cls.db_path_list = ['/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_1.feature', '/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_2.feature', '/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_3.feature', '/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_4.feature']
-config.cls.map_locs = [{'cuda:0':'cuda:1'}, {'cuda:0':'cuda:1'}, {'cuda:1':'cuda:0'}, {'cuda:1':'cuda:0'}]
-config.cls.np_path_list = ['/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_1.feature.npz', '/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_2.feature.npz', '/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_3.feature.npz', '/gemfield/hostpv/gemfield/deepvac-service/src/db/asia_emor_fix_merged_4.feature.npz']
+config.cls.class_num = 90219  # numbers of idx
+config.cls.log_every = 10000 # log
+config.cls.db_path_list = [] # your db_paths list
+config.cls.np_path_list = []  # your np_paths list
+config.cls.map_locs = [{'cuda:0':'cuda:1'}] # your map_locs config
+config.cls.file_path = './test_report.txt'  # result file path
+##########classifier_faiss report########
+config.cls_faiss = AttrDict()
+config.cls_faiss.class_num = 90219 # numbers of idx
+config.cls_faiss.log_every = 10000 # log
+config.cls_faiss.db_path_list = [] # your db_paths list
+config.cls_faiss.np_path_list = [] # your np_paths list
+config.cls_faiss.file_path = './test_report.txt' # result file path
+##########classifier_faiss_pth report########
+config.cls_faiss_pth = AttrDict()
+config.cls_faiss_pth.class_num = 90219  # numbers of idx
+config.cls_faiss_pth.log_every = 10000  # log
+config.cls_faiss_pth.db_path_list = []   # your db_paths list
+config.cls_faiss_pth.np_path_list = []   # your np_paths list
+config.cls_faiss_pth.map_locs = [{'cuda:0':'cuda:1'}] # your map_locs config
+config.cls_faiss_pth.file_path = './test_report.txt' # result file path
---
+++ examples/getDatasetReapt/config.py
@@ -0,0 +1,6 @@
+import os
+from deepvac.syszux_config import *
+##########classifier report########
+config.repeat = AttrDict()
+config.repeat.input_path = '/ your input file path /'
+config.repeat.output_path = '/ your output file path /'
