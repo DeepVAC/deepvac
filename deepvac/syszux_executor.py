666a7f8ecf433420a04d1364b778c51dfbddb43b
Gemfield <gemfield@civilnet.cn>
2020-11-19 15:38:20 +0800

    fix the wrong dependency on synthesis factory

--- deepvac/syszux_executor.py
+++ deepvac/syszux_executor.py
@@ -3,7 +3,6 @@ from collections import OrderedDict
 from collections import defaultdict
 from .syszux_aug_factory import AugFactory
 from .syszux_loader_factory import LoaderFactory,DatasetFactory
-from .syszux_synthesis_factory import SynthesisFactory
 from .syszux_log import LOG
 import cv2
 import re
