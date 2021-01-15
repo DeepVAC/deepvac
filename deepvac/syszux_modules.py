6bed4c3bff18fc5cd53fc23484066da0e7909dad
Gemfield <gemfield@civilnet.cn>
2021-01-15 20:21:55 +0800

    Add syszux_mobilenet

--- deepvac/syszux_modules.py
+++ deepvac/syszux_modules.py
@@ -30,6 +30,19 @@ class Conv2dBNReLU(nn.Sequential):
             nn.BatchNorm2d(out_planes, momentum=0.1),
             nn.ReLU(inplace=False)
         )
+
+def initWeights(civilnet):
+    for m in civilnet.modules():
+        if isinstance(m, nn.Conv2d):
+            nn.init.kaiming_normal_(m.weight, mode='fan_out')
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.BatchNorm2d):
+            nn.init.ones_(m.weight)
+            nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.Linear):
+            nn.init.normal_(m.weight, 0, 0.01)
+            nn.init.zeros_(m.bias)
 # enhanced CNN END
 
 # introduced by mobilenet series BEGIN
