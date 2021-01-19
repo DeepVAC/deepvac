8cd8f816aa49f802b633eea36988a72e042677b3
Gemfield <gemfield@civilnet.cn>
2021-01-19 20:41:16 +0800

    update syszux_modules to add yolo block

--- deepvac/syszux_modules.py
+++ deepvac/syszux_modules.py
@@ -22,8 +22,8 @@ class SELayer(nn.Module):
         return x * y
 
 class Conv2dBNReLU(nn.Sequential):
-    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, padding=-1, groups=1):
-        if padding == -1:
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, padding=None, groups=1):
+        if padding is None:
             padding = (kernel_size - 1) // 2
         super(Conv2dBNReLU, self).__init__(
             nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
@@ -43,6 +43,76 @@ def initWeights(civilnet):
         elif isinstance(m, nn.Linear):
             nn.init.normal_(m.weight, 0, 0.01)
             nn.init.zeros_(m.bias)
+
+class Conv2dBNHardswish(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, padding=None, groups=1):
+        if padding is None:
+            padding = (kernel_size - 1) // 2
+        super(Conv2dBNHardswish, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_planes, momentum=0.1),
+            nn.Hardswish()
+        )
+
+class BottleneckStd(nn.Module):
+    # Standard bottleneck
+    def __init__(self, in_planes, out_planes, groups=1, shortcut=True, expansion=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+        super(Bottleneck, self).__init__()
+        hidden_planes = int(out_planes * expansion)  # hidden channels
+        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1)
+        self.conv2 = Conv2dBNHardswish(hidden_planes, out_planes, 3, 1, groups=groups)
+        self.add = shortcut and in_planes == out_planes
+
+    def forward(self, x):
+        return x + self.conv2(self.conv1(x)) if self.add else self.conv2(self.conv1(x))
+
+class BottleneckCSP(nn.Module):
+    def __init__(self, in_planes, out_planes, bottle_std_num=1, shortcut=True, groups=1, expansion=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(BottleneckCSP, self).__init__()
+        hidden_planes = int(out_planes * expansion)  # hidden channels
+        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1)
+        self.conv2 = nn.Conv2d(in_planes, hidden_planes, 1, 1, bias=False)
+        self.conv3 = nn.Conv2d(hidden_planes, hidden_planes, 1, 1, bias=False)
+        self.conv4 = Conv2dBNHardswish(2 * hidden_planes, out_planes, 1, 1)
+        self.bn = nn.BatchNorm2d(2 * hidden_planes)  # applied to cat(conv2, conv3)
+        self.act = nn.LeakyReLU(0.1, inplace=True)
+        self.std_bottleneck_list = nn.Sequential(*[BottleneckStd(hidden_planes, hidden_planes, groups=groups, shortcut=shortcut, expansion=1.0) for _ in range(bottle_std_num)])
+
+    def forward(self, x):
+        y1 = self.conv3(self.std_bottleneck_list(self.conv1(x)))
+        y2 = self.conv2(x)
+        return self.conv4(self.act(self.bn(torch.cat((y1, y2), dim=1))))
+
+class SPP(nn.Module):
+    # Spatial pyramid pooling layer used in YOLOv3-SPP
+    def __init__(self, in_planes, out_planes, pool_kernel_size=(5, 9, 13)):
+        super(SPP, self).__init__()
+        hidden_planes = in_planes // 2  # hidden channels
+        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1)
+        self.conv2 = Conv2dBNHardswish(hidden_planes * (len(pool_kernel_size) + 1), out_planes, 1, 1)
+        self.pool_list = nn.ModuleList([nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2) for k in pool_kernel_size])
+
+    def forward(self, x):
+        x = self.conv1(x)
+        return self.conv2(torch.cat([x] + [pool(x) for pool in self.pool_list], 1))
+
+class Focus(nn.Module):
+    # Focus wh information into c-space
+    def __init__(self, in_planes, out_planes, kernel_size=1, stride=1, padding=None, groups=1):
+        super(Focus, self).__init__()
+        self.conv = Conv2dBNHardswish(in_planes * 4, out_planes, kernel_size, stride, padding, groups)
+
+    def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
+        return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
+
+class Concat(nn.Module):
+    # Concatenate a list of tensors along dimension
+    def __init__(self, dimension=1):
+        super(Concat, self).__init__()
+        self.d = dimension
+
+    def forward(self, x):
+        return torch.cat(x, self.d)
 # enhanced CNN END
 
 # introduced by mobilenet series BEGIN
@@ -74,8 +144,8 @@ class hsigmoid(nn.Module):
         return out
 
 class Conv2dBNHswish(nn.Sequential):
-    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, groups=1, padding=-1):
-        if padding == -1:
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, padding=None, groups=1):
+        if padding is None:
             padding = (kernel_size - 1) // 2
         super(Conv2dBNHswish, self).__init__(
             nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
@@ -84,9 +154,9 @@ class Conv2dBNHswish(nn.Sequential):
         )
 
 class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, kernel_size, stride, expand_ratio, use_se, use_hs, padding=-1):
+    def __init__(self, inp, oup, kernel_size, stride, expand_ratio, use_se, use_hs, padding=None):
         super(InvertedResidual, self).__init__()
-        if padding == -1:
+        if padding is None:
             padding = (kernel_size - 1) // 2
         hidden_dim = makeDivisible(inp * expand_ratio, 8)
         assert stride in [1, 2, (2, 1)]
