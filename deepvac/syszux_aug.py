e8243c72d445b5799b1e09b69cd5f739e6557df5
wyh163 <SIAKAM@bupt.edu.cn>
2021-01-26 14:32:23 +0800

    add retina modules

--- deepvac/syszux_aug.py
+++ deepvac/syszux_aug.py
@@ -853,3 +853,243 @@ class VFlipAug(AugBase):
         if label.size:
             label[:, 2] = 1 - label[:, 2]
         return img, label
+
+class RetinaCropAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(RetinaCropAug, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        pass
+    
+    def _matrix_iof(self, a, b):
+        lt = np.maximum(a[:, np.newaxis, :2], b[:, :2])
+        rb = np.minimum(a[:, np.newaxis, 2:], b[:, 2:])
+        area_i = np.prod(rb - lt, axis=2) * (lt < rb).all(axis=2)
+        area_a = np.prod(a[:, 2:] - a[:, :2], axis=1)
+        return area_i / np.maximum(area_a[:, np.newaxis], 1)
+    
+    def __call__(self, image, boxes, labels, landm, img_dim):
+        height, width, _ = image.shape
+        pad_image_flag = True
+
+        for _ in range(250):
+
+            PRE_SCALES = [0.3, 0.45, 0.6, 0.8, 1.0]
+            scale = random.choice(PRE_SCALES)
+            short_side = min(width, height)
+            w = int(scale * short_side)
+            h = w
+
+            if width == w:
+                l = 0
+            else:
+                l = random.randrange(width - w)
+            if height == h:
+                t = 0
+            else:
+                t = random.randrange(height - h)
+            roi = np.array((l, t, l + w, t + h))
+
+            value = self._matrix_iof(boxes, roi[np.newaxis])
+            flag = (value >= 1)
+            if not flag.any():
+                continue
+
+            centers = (boxes[:, :2] + boxes[:, 2:]) / 2
+            mask_a = np.logical_and(roi[:2] < centers, centers < roi[2:]).all(axis=1)
+            boxes_t = boxes[mask_a].copy()
+            labels_t = labels[mask_a].copy()
+            landms_t = landm[mask_a].copy()
+            landms_t = landms_t.reshape([-1, 5, 2])
+
+            if boxes_t.shape[0] == 0:
+                continue
+
+            image_t = image[roi[1]:roi[3], roi[0]:roi[2]]
+
+            boxes_t[:, :2] = np.maximum(boxes_t[:, :2], roi[:2])
+            boxes_t[:, :2] -= roi[:2]
+            boxes_t[:, 2:] = np.minimum(boxes_t[:, 2:], roi[2:])
+            boxes_t[:, 2:] -= roi[:2]
+
+            # landm
+            landms_t[:, :, :2] = landms_t[:, :, :2] - roi[:2]
+            landms_t[:, :, :2] = np.maximum(landms_t[:, :, :2], np.array([0, 0]))
+            landms_t[:, :, :2] = np.minimum(landms_t[:, :, :2], roi[2:] - roi[:2])
+            landms_t = landms_t.reshape([-1, 10])
+
+
+            # make sure that the cropped image contains at least one face > 16 pixel at training image scale
+            b_w_t = (boxes_t[:, 2] - boxes_t[:, 0] + 1) / w * img_dim
+            b_h_t = (boxes_t[:, 3] - boxes_t[:, 1] + 1) / h * img_dim
+            mask_b = np.minimum(b_w_t, b_h_t) > 0.0
+            boxes_t = boxes_t[mask_b]
+            labels_t = labels_t[mask_b]
+            landms_t = landms_t[mask_b]
+
+            if boxes_t.shape[0] == 0:
+                continue
+
+            pad_image_flag = False
+
+            return image_t, boxes_t, labels_t, landms_t, pad_image_flag
+        return image, boxes, labels, landm, pad_image_flag
+
+class RetinaDistortAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(RetinaDistortAug, self).__init__(deepvac_config)
+
+    def _convert(self, image, alpha=1, beta=0):
+        tmp = image.astype(float) * alpha + beta
+        tmp[tmp < 0] = 0
+        tmp[tmp > 255] = 255
+        image[:] = tmp
+
+    def auditConfig(self):
+        pass
+
+    def __call__(self, image):
+        image = image.copy()
+
+        if random.randrange(2):
+
+            #brightness distortion
+            if random.randrange(2):
+                self._convert(image, beta=random.uniform(-32, 32))
+
+            #contrast distortion
+            if random.randrange(2):
+                self._convert(image, alpha=random.uniform(0.5, 1.5))
+
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            #saturation distortion
+            if random.randrange(2):
+                self._convert(image[:, :, 1], alpha=random.uniform(0.5, 1.5))
+
+            #hue distortion
+            if random.randrange(2):
+                tmp = image[:, :, 0].astype(int) + random.randint(-18, 18)
+                tmp %= 180
+                image[:, :, 0] = tmp
+
+            image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+        else:
+
+            #brightness distortion
+            if random.randrange(2):
+                self._convert(image, beta=random.uniform(-32, 32))
+
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            #saturation distortion
+            if random.randrange(2):
+                self._convert(image[:, :, 1], alpha=random.uniform(0.5, 1.5))
+
+            #hue distortion
+            if random.randrange(2):
+                tmp = image[:, :, 0].astype(int) + random.randint(-18, 18)
+                tmp %= 180
+                image[:, :, 0] = tmp
+
+            image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+
+            #contrast distortion
+            if random.randrange(2):
+                self._convert(image, alpha=random.uniform(0.5, 1.5))
+
+        return image
+
+class RetinaExpandAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(RetinaExpandAug, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        pass
+
+    def __call__(self, image, boxes, fill, p):
+        if random.randrange(2):
+            return image, boxes
+
+        height, width, depth = image.shape
+
+        scale = random.uniform(1, p)
+        w = int(scale * width)
+        h = int(scale * height)
+
+        left = random.randint(0, w - width)
+        top = random.randint(0, h - height)
+
+        boxes_t = boxes.copy()
+        boxes_t[:, :2] += (left, top)
+        boxes_t[:, 2:] += (left, top)
+        expand_image = np.empty(
+            (h, w, depth),
+            dtype=image.dtype)
+        expand_image[:, :] = fill
+        expand_image[top:top + height, left:left + width] = image
+        image = expand_image
+
+        return image, boxes_t
+
+class RetinaMirrorAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(RetinaMirrorAug, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        pass
+
+    def __call__(self, image, boxes, landms):
+        _, width, _ = image.shape
+        if random.randrange(2):
+            image = image[:, ::-1]
+            boxes = boxes.copy()
+            boxes[:, 0::2] = width - boxes[:, 2::-2]
+
+            # landm
+            landms = landms.copy()
+            landms = landms.reshape([-1, 5, 2])
+            landms[:, :, 0] = width - landms[:, :, 0]
+            tmp = landms[:, 1, :].copy()
+            landms[:, 1, :] = landms[:, 0, :]
+            landms[:, 0, :] = tmp
+            tmp1 = landms[:, 4, :].copy()
+            landms[:, 4, :] = landms[:, 3, :]
+            landms[:, 3, :] = tmp1
+            landms = landms.reshape([-1, 10])
+
+        return image, boxes, landms
+
+class RetinaPad2SquareAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(RetinaPad2SquareAug, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        pass
+
+    def __call__(self, image, rgb_mean, pad_image_flag):
+        if not pad_image_flag:
+            return image
+        height, width, _ = image.shape
+        long_side = max(width, height)
+        image_t = np.empty((long_side, long_side, 3), dtype=image.dtype)
+        image_t[:, :] = rgb_mean
+        image_t[0:0 + height, 0:0 + width] = image
+        return image_t
+
+class RetinaResizeSubtractMeanAug(AugBase):
+    def __init__(self, deepvac_config):
+        super(RetinaResizeSubtractMeanAug, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        pass
+
+    def __call__(self, image, insize, rgb_mean):
+        interp_methods = [cv2.INTER_LINEAR, cv2.INTER_CUBIC, cv2.INTER_AREA, cv2.INTER_NEAREST, cv2.INTER_LANCZOS4]
+        interp_method = interp_methods[random.randrange(5)]
+        image = cv2.resize(image, (insize, insize), interpolation=interp_method)
+        image = image.astype(np.float32)
+        image -= rgb_mean
+        return image.transpose(2, 0, 1)
+
--- deepvac/syszux_modules.py
+++ deepvac/syszux_modules.py
@@ -295,5 +295,126 @@ class DepthWiseConv2d(nn.Module):
             x = identity + x
         return x
 
+def conv_bn(inp, oup, stride = 1, leaky = 0):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.LeakyReLU(negative_slope=leaky, inplace=True)
+    )
 
+def conv_bn_no_relu(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+    )
 
+def conv_bn1X1(inp, oup, stride, leaky=0):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 1, stride, padding=0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.LeakyReLU(negative_slope=leaky, inplace=True)
+    )
+
+def conv_dw(inp, oup, stride, leaky=0.1):
+    return nn.Sequential(
+        nn.Conv2d(inp, inp, 3, stride, 1, groups=inp, bias=False),
+        nn.BatchNorm2d(inp),
+        nn.LeakyReLU(negative_slope= leaky,inplace=True),
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.LeakyReLU(negative_slope= leaky,inplace=True),        
+    )
+
+class SSH(nn.Module):
+    def __init__(self, in_channel, out_channel):
+        super(SSH, self).__init__()
+        assert out_channel % 4 == 0
+        leaky = 0
+        if (out_channel <= 64):
+            leaky = 0.1
+        self.conv3X3 = conv_bn_no_relu(in_channel, out_channel//2, stride=1)
+
+        self.conv5X5_1 = conv_bn(in_channel, out_channel//4, stride=1, leaky = leaky)
+        self.conv5X5_2 = conv_bn_no_relu(out_channel//4, out_channel//4, stride=1)
+
+        self.conv7X7_2 = conv_bn(out_channel//4, out_channel//4, stride=1, leaky = leaky)
+        self.conv7x7_3 = conv_bn_no_relu(out_channel//4, out_channel//4, stride=1)
+
+    def forward(self, input):
+        conv3X3 = self.conv3X3(input)
+
+        conv5X5_1 = self.conv5X5_1(input)
+        conv5X5 = self.conv5X5_2(conv5X5_1)
+
+        conv7X7_2 = self.conv7X7_2(conv5X5_1)        
+        conv7X7 = self.conv7x7_3(conv7X7_2)
+
+        out = torch.cat([conv3X3, conv5X5, conv7X7], dim=1)
+        out = F.relu(out)
+        return out
+
+class FPN(nn.Module):
+    def __init__(self,in_channels_list,out_channels):
+        super(FPN,self).__init__()
+        leaky = 0
+        
+        if (out_channels <= 64):
+            leaky = 0.1
+        self.output1 = conv_bn1X1(in_channels_list[0], out_channels, stride = 1, leaky = leaky)            
+        self.output2 = conv_bn1X1(in_channels_list[1], out_channels, stride = 1, leaky = leaky)
+        self.output3 = conv_bn1X1(in_channels_list[2], out_channels, stride = 1, leaky = leaky)
+
+        self.merge1 = conv_bn(out_channels, out_channels, leaky = leaky)
+        self.merge2 = conv_bn(out_channels, out_channels, leaky = leaky)
+
+    def forward(self, input):
+        names = list(input.keys())
+        input = list(input.values())
+
+        output1 = self.output1(input[0])
+        output2 = self.output2(input[1])        
+        output3 = self.output3(input[2])
+
+        up3 = F.interpolate(output3, size=[output2.size(2), output2.size(3)], mode="nearest")
+        output2 = output2 + up3        
+        output2 = self.merge2(output2)
+
+        up2 = F.interpolate(output2, size=[output1.size(2), output1.size(3)], mode="nearest")
+        output1 = output1 + up2
+        output1 = self.merge1(output1)
+        out = [output1, output2, output3]
+        return out
+
+class ClassHead(nn.Module):
+    def __init__(self,inchannels=512,num_anchors=3):
+        super(ClassHead,self).__init__()
+        self.num_anchors = num_anchors
+        self.conv1x1 = nn.Conv2d(inchannels,self.num_anchors*2,kernel_size=(1,1),stride=1,padding=0)
+
+    def forward(self,x):
+        out = self.conv1x1(x)
+        out = out.permute(0,2,3,1).contiguous()
+        
+        return out.view(out.shape[0], -1, 2)
+
+class BboxHead(nn.Module):
+    def __init__(self,inchannels=512,num_anchors=3):
+        super(BboxHead,self).__init__()
+        self.conv1x1 = nn.Conv2d(inchannels,num_anchors*4,kernel_size=(1,1),stride=1,padding=0)
+
+    def forward(self,x):
+        out = self.conv1x1(x)
+        out = out.permute(0,2,3,1).contiguous()
+
+        return out.view(out.shape[0], -1, 4)
+
+class LandmarkHead(nn.Module):
+    def __init__(self,inchannels=512,num_anchors=3):
+        super(LandmarkHead,self).__init__()
+        self.conv1x1 = nn.Conv2d(inchannels,num_anchors*10,kernel_size=(1,1),stride=1,padding=0)
+
+    def forward(self,x):
+        out = self.conv1x1(x)
+        out = out.permute(0,2,3,1).contiguous()
+
+        return out.view(out.shape[0], -1, 10)
