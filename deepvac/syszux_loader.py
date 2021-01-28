27fe909dbe62c8587500e45ac06e0dc70276582e
__MHGL__ <49035645+MHGL@users.noreply.github.com>
2021-01-28 14:38:29 +0800

    deepvac for yolov5 (#58)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- deepvac/syszux_loader.py
+++ deepvac/syszux_loader.py
@@ -86,9 +86,71 @@ class OsWalkerLoader(object):
     def __call__(self, input_dir=None):
         if input_dir:
             self.input_dir = input_dir
-            
+
         for subdir, dirs, files in os.walk(self.input_dir):
             for file in files:
                 #print os.path.join(subdir, file)
                 filepath = subdir + os.sep + file
                 yield filepath
+
+
+class CocoCVDataset(Dataset):
+    def __init__(self, deepvac_config):
+        super(CocoCVDataset, self).__init__()
+        self.conf = deepvac_config
+        self.transform = deepvac_config.transform
+        self.img_folder = deepvac_config.img_folder
+        self.aug_executor = deepvac_config.aug_executor
+
+        try:
+            from pycocotools.coco import COCO
+        except:
+            raise Exception("pycocotools module not found, you should try 'pip3 install pycocotools' first!")
+        self.coco = COCO(deepvac_config.annotation)
+        self.ids = list(sorted(self.coco.imgs.keys()))
+        self.cats = list(sorted(self.coco.cats.keys()))
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __getitem__(self, index):
+        sample, label = self._getSample(index)
+        # augment for numpy
+        if self.aug_executor:
+            sample = self.aug_executor(sample)
+        # pytorch offical transform for PIL Image or numpy
+        if self.transform is not None:
+            # rewrite method: '_buildSample' first
+            sample = self.transform(sample)
+        # post process
+        img, label = self._buildSample(sample, label)
+        return img, label
+
+    def _getSample(self, index):
+        img = self.loadImgs(index)
+        category_ids, boxes, masks = self.loadAnns(index)
+        # return target you want
+        return img, category_ids
+
+    def _buildSample(self, img, target):
+        # BGR -> RGB && (H, W, C) -> (C, H, W)
+        img = img[:, :, ::-1].transpose(2, 0, 1)
+        img = np.ascontiguousarray(img)
+        # numpy -> tensor
+        img = torch.from_numpy(img)
+        target = torch.from_numpy(target)
+        return img, target
+
+    def loadImgs(self, index):
+        file_name = self.coco.loadImgs(self.ids[index])[0]['file_name']
+        img = cv2.imread(os.path.join(self.img_folder, file_name), 1)
+        assert img is not None, "Image {} not found!".format(file_name)
+        return img
+
+    def loadAnns(self, index):
+        ann_ids = self.coco.getAnnIds(imgIds=self.ids[index])
+        anns = self.coco.loadAnns(ann_ids)
+        category_ids = np.array([self.cats.index(i["category_id"]) for i in anns], dtype=np.float)
+        boxes = np.array([i["bbox"] for i in anns], dtype=np.float)
+        masks = np.array([self.coco.annToMask(i) for i in anns], dtype=np.float)
+        return category_ids, boxes, masks
--- deepvac/syszux_modules.py
+++ deepvac/syszux_modules.py
@@ -65,38 +65,39 @@ def initWeightsNormal(civilnet):
             nn.init.zeros_(m.bias)
 
 class Conv2dBNHardswish(nn.Sequential):
-    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, padding=None, groups=1):
+    def __init__(self, in_planes, out_planes, kernel_size=3, stride=1, eps=1e-5, momentum=0.1, padding=None, groups=1):
         if padding is None:
             padding = (kernel_size - 1) // 2
         super(Conv2dBNHardswish, self).__init__(
             nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
-            nn.BatchNorm2d(out_planes, momentum=0.1),
+            nn.BatchNorm2d(out_planes, eps=eps, momentum=momentum),
             nn.Hardswish()
         )
 
+
 class BottleneckStd(nn.Module):
     # Standard bottleneck
-    def __init__(self, in_planes, out_planes, groups=1, shortcut=True, expansion=0.5):  # ch_in, ch_out, shortcut, groups, expansion
-        super(Bottleneck, self).__init__()
+    def __init__(self, in_planes, out_planes, groups=1, shortcut=True, eps=1e-5, momentum=0.1, expansion=0.5):  # ch_in, ch_out, shortcut, groups, expansion
+        super(BottleneckStd, self).__init__()
         hidden_planes = int(out_planes * expansion)  # hidden channels
-        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1)
-        self.conv2 = Conv2dBNHardswish(hidden_planes, out_planes, 3, 1, groups=groups)
+        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1, eps, momentum)
+        self.conv2 = Conv2dBNHardswish(hidden_planes, out_planes, 3, 1, eps, momentum, groups=groups)
         self.add = shortcut and in_planes == out_planes
 
     def forward(self, x):
         return x + self.conv2(self.conv1(x)) if self.add else self.conv2(self.conv1(x))
 
 class BottleneckCSP(nn.Module):
-    def __init__(self, in_planes, out_planes, bottle_std_num=1, shortcut=True, groups=1, expansion=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+    def __init__(self, in_planes, out_planes, bottle_std_num=1, shortcut=True, eps=1e-5, momentum=0.1, groups=1, expansion=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
         super(BottleneckCSP, self).__init__()
         hidden_planes = int(out_planes * expansion)  # hidden channels
-        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1)
+        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1, eps, momentum)
         self.conv2 = nn.Conv2d(in_planes, hidden_planes, 1, 1, bias=False)
         self.conv3 = nn.Conv2d(hidden_planes, hidden_planes, 1, 1, bias=False)
-        self.conv4 = Conv2dBNHardswish(2 * hidden_planes, out_planes, 1, 1)
-        self.bn = nn.BatchNorm2d(2 * hidden_planes)  # applied to cat(conv2, conv3)
+        self.conv4 = Conv2dBNHardswish(2 * hidden_planes, out_planes, 1, 1, eps, momentum)
+        self.bn = nn.BatchNorm2d(2 * hidden_planes, eps=eps, momentum=momentum)  # applied to cat(conv2, conv3)
         self.act = nn.LeakyReLU(0.1, inplace=True)
-        self.std_bottleneck_list = nn.Sequential(*[BottleneckStd(hidden_planes, hidden_planes, groups=groups, shortcut=shortcut, expansion=1.0) for _ in range(bottle_std_num)])
+        self.std_bottleneck_list = nn.Sequential(*[BottleneckStd(hidden_planes, hidden_planes, groups=groups, shortcut=shortcut, eps=eps, momentum=momentum, expansion=1.0) for _ in range(bottle_std_num)])
 
     def forward(self, x):
         y1 = self.conv3(self.std_bottleneck_list(self.conv1(x)))
@@ -105,11 +106,11 @@ class BottleneckCSP(nn.Module):
 
 class SPP(nn.Module):
     # Spatial pyramid pooling layer used in YOLOv3-SPP
-    def __init__(self, in_planes, out_planes, pool_kernel_size=(5, 9, 13)):
+    def __init__(self, in_planes, out_planes, pool_kernel_size=(5, 9, 13), eps=1e-5, momentum=0.1):
         super(SPP, self).__init__()
         hidden_planes = in_planes // 2  # hidden channels
-        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1)
-        self.conv2 = Conv2dBNHardswish(hidden_planes * (len(pool_kernel_size) + 1), out_planes, 1, 1)
+        self.conv1 = Conv2dBNHardswish(in_planes, hidden_planes, 1, 1, eps, momentum)
+        self.conv2 = Conv2dBNHardswish(hidden_planes * (len(pool_kernel_size) + 1), out_planes, 1, 1, eps, momentum)
         self.pool_list = nn.ModuleList([nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2) for k in pool_kernel_size])
 
     def forward(self, x):
@@ -118,9 +119,9 @@ class SPP(nn.Module):
 
 class Focus(nn.Module):
     # Focus wh information into c-space
-    def __init__(self, in_planes, out_planes, kernel_size=1, stride=1, padding=None, groups=1):
+    def __init__(self, in_planes, out_planes, kernel_size=1, stride=1, eps=1e-5, momentum=0.1, padding=None, groups=1):
         super(Focus, self).__init__()
-        self.conv = Conv2dBNHardswish(in_planes * 4, out_planes, kernel_size, stride, padding, groups)
+        self.conv = Conv2dBNHardswish(in_planes * 4, out_planes, kernel_size, stride, eps, momentum, padding, groups)
 
     def forward(self, x):  # x(b,c,w,h) -> y(b,4c,w/2,h/2)
         return self.conv(torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1))
@@ -186,7 +187,7 @@ class InvertedResidual(nn.Module):
         layers = []
         if expand_ratio != 1:
             layers.append(Conv2dBNHswish(inp, hidden_dim, kernel_size=1))  # pw
-        
+
         layers.extend([
             # dw
             nn.Conv2d(hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim, bias=False),
@@ -274,9 +275,9 @@ class BottleneckIR(nn.Module):
 
         self.res_layer = nn.Sequential(
             nn.BatchNorm2d(inplanes),
-            nn.Conv2d(inplanes, outplanes, kernel_size=3, stride=1, padding=1 ,bias=False), 
+            nn.Conv2d(inplanes, outplanes, kernel_size=3, stride=1, padding=1 ,bias=False),
             nn.PReLU(outplanes),
-            nn.Conv2d(outplanes, outplanes, kernel_size=3, stride=stride, padding=1 ,bias=False), 
+            nn.Conv2d(outplanes, outplanes, kernel_size=3, stride=stride, padding=1 ,bias=False),
             nn.BatchNorm2d(outplanes)
         )
 
@@ -294,7 +295,7 @@ class DepthWiseConv2d(nn.Module):
         self.conv3 = nn.Conv2d(groups, outplanes, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn = nn.BatchNorm2d(outplanes)
         self.residual = residual
-    
+
     def forward(self, x):
         identity = x
         x = self.conv1(x)
@@ -346,7 +347,7 @@ class SSH(nn.Module):
         conv5X5_1 = self.conv5X5_1(input)
         conv5X5 = self.conv5X5_2(conv5X5_1)
 
-        conv7X7_2 = self.conv7X7_2(conv5X5_1)        
+        conv7X7_2 = self.conv7X7_2(conv5X5_1)
         conv7X7 = self.conv7x7_3(conv7X7_2)
 
         out = torch.cat([conv3X3, conv5X5, conv7X7], dim=1)
@@ -369,11 +370,11 @@ class FPN(nn.Module):
         input = list(input.values())
 
         output1 = self.conv1(input[0])
-        output2 = self.conv2(input[1])        
+        output2 = self.conv2(input[1])
         output3 = self.conv3(input[2])
 
         up3 = F.interpolate(output3, size=[output2.size(2), output2.size(3)], mode="nearest")
-        output2 = output2 + up3 
+        output2 = output2 + up3
         output2 = self.conv5(output2)
 
         up2 = F.interpolate(output2, size=[output1.size(2), output1.size(3)], mode="nearest")
