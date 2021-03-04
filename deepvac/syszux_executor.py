db8dc513725cd66433b1f1ec60a2d9b9486552d9
MHGL <1545989994@qq.com>
2021-03-04 17:00:23 +0800

    add auditUserConfig

--- deepvac/syszux_executor.py
+++ deepvac/syszux_executor.py
@@ -177,15 +177,17 @@ class SegImageAugExecutor(Executor):
     def __init__(self, deepvac_config):
         super(SegImageAugExecutor, self).__init__(deepvac_config)
         ac1 = AugChain('GaussianAug || RandomColorJitterAug || BrightnessJitterAug || ContrastJitterAug || MotionAug',deepvac_config)
+        ac2 = AugChain('MotionAug',deepvac_config)
         self.addAugChain('ac1', ac1, 1)
+        self.addAugChain('ac2', ac1, 0.2)
 
 class SegImageWithMaskAugExecutor(Executor):
     def __init__(self, deepvac_config):
         super(SegImageWithMaskAugExecutor, self).__init__(deepvac_config)
         ac1 = AugChain('ImageWithMasksRandomCropAug',deepvac_config)
         ac2 = AugChain('ImageWithMasksRandomHorizontalFlipAug || ImageWithMasksRandomRotateAug',deepvac_config)
-        self.addAugChain('ac1', ac1, 1)
-        self.addAugChain('ac2', ac2, 1)
+        self.addAugChain('ac1', ac1, 0.4)
+        self.addAugChain('ac2', ac2, 0.6)
 
 if __name__ == "__main__":
     x = Chain("RandomColorJitterAug@0.3 => MosaicAug@0.8 => MotionAug ")
