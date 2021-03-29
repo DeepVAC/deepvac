d798428966968b89806b54d317246d6936d76795
lihang <lihang@deepvac.com>
2021-03-29 15:27:32 +0800

    update regnet

--- deepvac/syszux_regnet.py
+++ deepvac/syszux_regnet.py
@@ -3,50 +3,53 @@ import torch.nn as nn
 import torch
 
 #function begin
-def quantize_float(f, divisor):
-    """Converts a float to closest non-zero int divisible by divisor."""
-    return int(round(f / divisor) * divisor)
 
-
-def adjust_ws_gs_comp(stage_channel_num, bot_muls_per_stage, group_w_per_stage):
-    """Adjusts the compatibility of widths and groups."""
-    ws_bot = [int(w * b) for w, b in zip(stage_channel_num, bot_muls_per_stage)]
-    group_w_per_stage = [min(g, w_bot) for g, w_bot in zip(group_w_per_stage, ws_bot)]
-    ws_bot = [quantize_float(w_bot, g) for w_bot, g in zip(ws_bot, group_w_per_stage)]
-    stage_channel_num = [int(w_bot / b) for w_bot, b in zip(ws_bot, bot_muls_per_stage)]
-    return stage_channel_num, group_w_per_stage
-
-
-def get_stages_from_blocks(stage_channel_num, rs):
-    """Gets stage_channel_num/ds of network at each stage from per block values."""
-    ts_temp = zip(stage_channel_num + [0], [0] + stage_channel_num, rs + [0], [0] + rs)
-    ts = [w != wp or r != rp for w, wp, r, rp in ts_temp]
-    s_ws = [w for w, t in zip(stage_channel_num, ts[:-1]) if t]
-    s_ds = np.diff([d for d, t in zip(range(len(ts)), ts) if t]).tolist()
-    return s_ws, s_ds
-
+def adjust_block_compatibility(width_per_stage, bot_muls_per_stage, group_w_per_stage):
+    """Adjusts the compatibility of widths, bottlenecks, and groups."""
+    assert len(width_per_stage) == len(bot_muls_per_stage) == len(group_w_per_stage)
+    assert all(w > 0 and b > 0 and g > 0 for w, b, g in zip(width_per_stage, bot_muls_per_stage, group_w_per_stage))
+    assert all(b < 1 or b % 1 == 0 for b in bot_muls_per_stage)
+    vs = [int(max(1, w * b)) for w, b in zip(width_per_stage, bot_muls_per_stage)]
+    group_w_per_stage = [int(min(g, v)) for g, v in zip(group_w_per_stage, vs)]
+    ms = [np.lcm(g, int(b)) if b > 1 else g for g, b in zip(group_w_per_stage, bot_muls_per_stage)]
+    vs = [max(m, int(round(v / m) * m)) for v, m in zip(vs, ms)]
+    width_per_stage = [int(v / b) for v, b in zip(vs, bot_muls_per_stage)]
+    assert all(w * b % g == 0 for w, b, g in zip(width_per_stage, bot_muls_per_stage, group_w_per_stage))
+    return width_per_stage, bot_muls_per_stage, group_w_per_stage
 
 def generate_regnet(anynet_slope, anynet_initial_width, channel_controller, depth, divisor=8):
-    """Generates per block stage_channel_num from RegNet parameters."""
+    """Generates per stage widths and depths from RegNet parameters."""
     assert anynet_slope >= 0 and anynet_initial_width > 0 and channel_controller > 1 and anynet_initial_width % divisor == 0
+    # Generate continuous per-block ws
     ws_cont = np.arange(depth) * anynet_slope + anynet_initial_width
+    # Generate quantized per-block ws
     ks = np.round(np.log(ws_cont / anynet_initial_width) / np.log(channel_controller))
-    stage_channel_num = anynet_initial_width * np.power(channel_controller, ks)
-    stage_channel_num = np.round(np.divide(stage_channel_num, divisor)) * divisor
-    num_stages, max_stage = len(np.unique(stage_channel_num)), ks.max() + 1
-    stage_channel_num, ws_cont = stage_channel_num.astype(int).tolist(), ws_cont.tolist()
-    return stage_channel_num, num_stages, max_stage, ws_cont
-#function end
+    ws_all = anynet_initial_width * np.power(channel_controller, ks)
+    ws_all = np.round(np.divide(ws_all, divisor)).astype(int) * divisor
+    # Generate per stage ws and ds (assumes ws_all are sorted)
+    ws, ds = np.unique(ws_all, return_counts=True)
+    # Compute number of actual stages and total possible stages
+    num_stages, total_stages = len(ws), ks.max() + 1
+    # Convert numpy arrays to lists and return
+    ws, ds, ws_all, ws_cont = (x.tolist() for x in (ws, ds, ws_all, ws_cont))
+    return ws, ds, num_stages, total_stages, ws_all, ws_cont
 
 class AnyHead(nn.Module):
     """AnyNet head: AvgPool, 1x1."""
 
-    def __init__(self, in_planes, class_num):
+    def __init__(self, in_planes, class_num, head_w):
         super(AnyHead, self).__init__()
+        self.head_w = head_w
+        if head_w>0:
+            self.conv = nn.Conv2d(in_planes, head_w, 1, stride=1, padding=0, groups=1, bias=False)
+            self.bn = nn.BatchNorm2d(head_w, eps=1e-5, momentum=0.1)
+            self.relu = nn.ReLU(inplace=True)
+            in_planes = head_w
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(in_planes, class_num, bias=True)
 
     def forward(self, x):
+        x = self.relu(self.bn(self.conv(x))) if self.head_w > 0 else x
         x = self.avg_pool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
@@ -145,24 +148,15 @@ class AnyStage(nn.Module):
         return x
 
 class RegNet(nn.Module):
-    def __init__(self, anynet_slope, anynet_initial_width, channel_controller, depth, group_w, class_num=0):
+    def __init__(self, anynet_slope, anynet_initial_width, channel_controller, depth, group_w, class_num=0, head_w=0):
         super(RegNet, self).__init__()
         stem_w = 32
         se_ratio = 0.25
-        stage_channel_num, num_stages, _, _ = generate_regnet(anynet_slope, anynet_initial_width, channel_controller, depth)
-        # Convert to per stage format
-        width_per_stage, depth_per_stage = get_stages_from_blocks(stage_channel_num, stage_channel_num)
-        # Use the same group_width, bottom_mul and ss for each stage
-        # GROUP_W: 8 for 200MF
-        group_w_per_stage = [group_w for _ in range(num_stages)]
-        bot_muls_per_stage = [1.0 for _ in range(num_stages)]
-        stride_per_stage = self.auditConfig(num_stages)
-        assert(len(stride_per_stage) == num_stages, "configure stride_per_stage error!")
-        # Adjust the compatibility of stage_channel_num and gws
-        width_per_stage, group_w_per_stage = adjust_ws_gs_comp(width_per_stage, bot_muls_per_stage, group_w_per_stage)
-        # Generate dummy bot muls and gs for models that do not use them
-        bot_muls_per_stage = bot_muls_per_stage if bot_muls_per_stage else [None for _d in depth_per_stage]
-        group_w_per_stage = group_w_per_stage if group_w_per_stage else [None for _d in depth_per_stage]
+        width_per_stage, depth_per_stage = generate_regnet(anynet_slope, anynet_initial_width, channel_controller, depth)[0:2]
+        group_w_per_stage = [group_w for _ in width_per_stage]
+        bot_muls_per_stage = [1.0 for _ in width_per_stage]
+        stride_per_stage = self.auditConfig(width_per_stage)
+        width_per_stage, bot_muls_per_stage, group_w_per_stage = adjust_block_compatibility(width_per_stage, bot_muls_per_stage, group_w_per_stage)
         stage_params = list(zip(depth_per_stage, width_per_stage, stride_per_stage, bot_muls_per_stage, group_w_per_stage))
 
         self.stem = SimpleStemIN(3, stem_w)
@@ -173,10 +167,10 @@ class RegNet(nn.Module):
             prev_w = w
 
         if class_num > 0:
-            self.head = AnyHead(in_planes=prev_w, class_num=class_num)
+            self.head = AnyHead(in_planes=prev_w, class_num=class_num, head_w=head_w)
     
-    def auditConfig(self, num_stages):
-        stride_per_stage = [2 for _ in range(num_stages)]
+    def auditConfig(self, width_per_stage):
+        stride_per_stage = [2 for _ in width_per_stage]
         return stride_per_stage
 
     def forward(self, x):
@@ -185,27 +179,27 @@ class RegNet(nn.Module):
         return x
 
 class RegNetSmall(RegNet):
-    def __init__(self, class_num=0):
-        super(RegNetSmall, self).__init__(27.89, 48, 2.09, 16, 8, class_num)
+    def __init__(self, class_num=0, head_w=0):
+        super(RegNetSmall, self).__init__(27.89, 48, 2.09, 16, 8, class_num, head_w)
 
 class RegNetMedium(RegNet):
-    def __init__(self, class_num=0):
-        super(RegNetMedium, self).__init__(20.71, 48, 2.65, 27, 24, class_num)
+    def __init__(self, class_num=0, head_w=0):
+        super(RegNetMedium, self).__init__(20.71, 48, 2.65, 27, 24, class_num, head_w)
 
 class RegNetLarge(RegNet):
-    def __init__(self, class_num=0):
-        super(RegNetLarge, self).__init__(31.41, 96, 2.24, 22, 64, class_num)
+    def __init__(self, class_num=0, head_w=0):
+        super(RegNetLarge, self).__init__(31.41, 96, 2.24, 22, 64, class_num, head_w)
 
 class RegNetOCR(RegNet):
-    def __init__(self, class_num=0):
-        super(RegNetOCR, self).__init__(27.89, 48, 2.09, 16, 8, class_num)
+    def __init__(self, class_num=0, head_w=0):
+        super(RegNetOCR, self).__init__(27.89, 48, 2.09, 16, 8, class_num, head_w)
 
-    def auditConfig(self, num_stages):
+    def auditConfig(self, width_per_stage):
         stride_per_stage = [(2,1),(2,1),(2,1),2]
         return stride_per_stage
 
 if __name__ == '__main__':
-    model = RegNetSmall(1000)
+    model = RegNetOCR()
     print(model)
     x = torch.randn((1,3,32,320))
     print(model(x).shape)
