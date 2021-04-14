366e9d6e648dda74440e933bcb393ff34e596b1a
buptlihang <18801481780@163.com>
2021-04-14 23:36:36 +0800

    upload dbloss

--- deepvac/syszux_loss.py
+++ deepvac/syszux_loss.py
@@ -17,6 +17,68 @@ class LossBase(object):
         raise Exception("Not implemented!")
 
 
+class MaskL1Loss(LossBase):
+    def __init__(self, deepvac_config):
+        super(MaskL1Loss, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.eps = 1e-6
+
+    def __call__(self, pred: torch.Tensor, gt, mask):
+        loss = (torch.abs(pred - gt) * mask).sum() / (mask.sum() + self.eps)
+        return loss
+
+class DiceLoss(LossBase):
+    def __init__(self, deepvac_config):
+        super(DiceLoss, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.eps = 1e-6
+
+    def __call__(self, pred: torch.Tensor, gt, mask, weights=None):
+        return self._compute(pred, gt, mask, weights)
+
+    def _compute(self, pred, gt, mask, weights):
+        if pred.dim() == 4:
+            pred = pred[:, 0, :, :]
+            gt = gt[:, 0, :, :]
+        assert pred.shape == gt.shape
+        assert pred.shape == mask.shape
+        if weights is not None:
+            assert weights.shape == mask.shape
+            mask = weights * mask
+        intersection = (pred * gt * mask).sum()
+        union = (pred * mask).sum() + (gt * mask).sum() + self.eps
+        loss = 1 - 2.0 * intersection / union
+        assert loss <= 1
+        return loss
+
+class BalanceCrossEntropyLoss(LossBase):
+    def __init__(self, deepvac_config):
+        super(BalanceCrossEntropyLoss, self).__init__(deepvac_config)
+
+    def auditConfig(self):
+        self.negative_ratio = 3.0
+        self.eps = 1e-6
+
+    def __call__(self,
+                pred: torch.Tensor,
+                gt: torch.Tensor,
+                mask: torch.Tensor,
+                return_origin=False):
+        positive = (gt * mask).byte()
+        negative = ((1 - gt) * mask).byte()
+        positive_count = int(positive.float().sum())
+        negative_count = min(int(negative.float().sum()), int(positive_count * self.negative_ratio))
+        loss = nn.functional.binary_cross_entropy(pred, gt, reduction='none')
+        positive_loss = loss * positive.float()
+        negative_loss = loss * negative.float()
+        negative_loss, _ = negative_loss.view(-1).topk(negative_count)
+        balance_loss = (positive_loss.sum() + negative_loss.sum()) / (positive_count + negative_count + self.eps)
+        if return_origin:
+            return balance_loss, loss
+        return balance_loss
+
 class BCEBlurWithLogitsLoss(LossBase):
     # BCEwithLogitLoss() with reduced missing label effects.
     def __init__(self, deepvac_config):
