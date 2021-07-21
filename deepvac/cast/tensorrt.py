460ba5049778fe9d8fc5978c7a6a8a7f1d021d45
MHGL <1545989994@qq.com>
2021-07-21 20:33:46 +0800

    add workspace size to tensorrt

--- deepvac/cast/tensorrt.py
+++ deepvac/cast/tensorrt.py
@@ -11,13 +11,13 @@ class TensorrtCast(DeepvacCast):
     def auditConfig(self):
         if not self.config.model_dir:
             return False
-    
+
         return True
-    
+
     def process(self, cast_output_file=None):
         try:
             import tensorrt as trt
-            initLog(trt)            
+            initLog(trt)
         except:
             LOG.logE("You must install tensorrt package if you want to convert pytorch to onnx. 1. Download Tensorrt7.2.3(for CUDA11.0) from https://developer.nvidia.com/tensorrt \
             2. unpack Tensorrt*.tar.gz  3. pip install tensorrt-x-cpx-none-linux_x86_64.whl in Tensorrt*(your_tensorrt_path)/python", exit=True)
@@ -26,7 +26,7 @@ class TensorrtCast(DeepvacCast):
         if cast_output_file:
             output_trt_file = '{}/trt__{}.trt'.format(self.trainer_config.output_dir, cast_output_file)
             self.config.model_dir = output_trt_file
-        
+
         LOG.logI("config.trt_model_dir found, save tensorrt model to {}...".format(self.config.model_dir))
 
         #to onnx, also set self.config.onnx_model_dir, self.config.onnx_input_names and self.config.onnx_output_names
@@ -36,6 +36,8 @@ class TensorrtCast(DeepvacCast):
             with open(self.config.onnx_model_dir, 'rb') as model:
                 parser.parse(model.read())
             config = builder.create_builder_config()
+            config.max_workspace_size = 1 << 30
+
             if self.config.onnx_dynamic_ax:
                 profile = builder.create_optimization_profile()
                 profile.set_shape(self.config.onnx_input_names[0], self.config.input_min_dims, self.config.input_opt_dims, self.config.input_max_dims)
